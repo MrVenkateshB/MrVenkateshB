d74f773a3a2f776f42d0a43923cb019cbf56fe38
MrVenkateshB <venkatesh.balabadruni92@gmail.com>
2021-09-21 23:06:39 +0530

    Conditional content rendering - #8

--- starting-setup/src/components/Expenses/Expenses.js
+++ starting-setup/src/components/Expenses/Expenses.js
@@ -12,24 +12,41 @@ function Expenses(props) {
     setSelectedYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  /** Conditonal rendering Alt-2 */
+  let expenseContent = <p>No Expense found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+  /** END conditional rendering Alt-2 */
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={selectedYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items
-        .filter(
-          (expense) => expense.date.getFullYear().toString() === selectedYear
-        )
-        .map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+      { // Conditional Rendering Alt - 1
+      /* {filteredExpenses.length === 0 && <p>No Expense found.</p>}
+      {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))} */}
+      {expenseContent}
     </Card>
   );
 }
