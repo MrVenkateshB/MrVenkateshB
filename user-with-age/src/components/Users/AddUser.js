59e0a4e4ae943bafadcc68a92d6392480a2e40b9
MrVenkateshB <venkatesh.balabadruni92@gmail.com>
2022-03-01 21:13:46 +0530

    Usage of useRefs in react- #16

--- user-with-age/src/components/Users/AddUser.js
+++ user-with-age/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -8,37 +8,35 @@ import classes from './AddUser.module.css';
 
 
 const AddUser = props => {
-    const [enteredUsername, setEnteredUserName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
             setError({
                 title: 'Invalid input!!',
                 msg: 'Please enter a valid name and age (non-empty values)'
             });
             return;
         }
-        if (+enteredAge < 1) {
+        if (+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age !!',
                 msg: 'Please enter a valid age (greater than 1)'
             });
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUserName('');
-        setEnteredAge('');
-    };
-
-    const userNameChangeHandler = (ev) => {
-        setEnteredUserName(ev.target.value);
-    };
-
-    const ageChangeHandler = (ev) => {
-        setEnteredAge(ev.target.value);
+        props.onAddUser(enteredName, enteredUserAge);
+        // Rarely use refs to manipulate the dom, this is not advised. You can use useState solution
+        // by setting up state, using setstate to empty values
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
 
     const errorHandler = () => {
@@ -57,9 +55,9 @@ const AddUser = props => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor='username'>UserName:</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={userNameChangeHandler}/>
+                    <input id="username" type="text" ref={nameInputRef}/>
                     <label htmlFor='age'>Age(Years):</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                    <input id="age" type="number" ref={ageInputRef}/>
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
